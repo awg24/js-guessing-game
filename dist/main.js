392793440e35e7f111885ab9e2d261b5b0800359
allen wes <allen.wes.g@gmail.com>
2015-05-13 11:53:41 -0500

    deleted a few more extra lines

--- dist/main.js
+++ dist/main.js
@@ -1,11 +1,6 @@
 function* guessingGame() {
-	//console.log(yield game.say('Hey there!'));
-	//console.log(yield game.say('Woah', 'This is cool'));
-	//console.log(yield game.ask('Woah', 'This is cool'));
-	//console.log(yield game.choose('Woah', 'This is cool', 'Hello world'));
-	//console.log(yield game.say('That\'s all folks!', 'THE END'));
-	do {
 
+	do {
 		var playAgain = "yes";
 		var number = Math.floor(Math.random()*100 + 1);
 		var myGuess = yield game.ask("I'm thinking of a number 1 through 100... What is it?");
