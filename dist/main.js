819a1c31e2c5a33f4fe3948835b618e0036f707d
allen wes <allen.wes.g@gmail.com>
2015-05-13 12:16:34 -0500

    added protection against not putting in a proper number

--- dist/main.js
+++ dist/main.js
@@ -3,13 +3,22 @@ function* guessingGame() {
 	do {
 		var playAgain = "yes";
 		var number = Math.floor(Math.random()*100 + 1);
+		console.log(number);
 		var myGuess = yield game.ask("I'm thinking of a number 1 through 100... What is it?");
-
+		while(isNaN(myGuess/2)){
+			myGuess = yield game.ask("You need to put in an actual number.. Guess again...");
+		}
 		while (myGuess != number){
 			if (myGuess < number){
 				myGuess = yield game.ask("Your guess was too low! Guess again...");
+				while(isNaN(myGuess/2)){
+					myGuess = yield game.ask("You need to put in an actual number.. Guess again...");
+				}
 			} else {
 				myGuess = yield game.ask(" Your guess was too high! Guess again..");
+				while(isNaN(myGuess/2)){
+					myGuess = yield game.ask("You need to put in an actual number.. Guess again...");
+				}
 			}
 		}
 
