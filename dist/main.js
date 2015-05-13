7751cca7cf4e8315610dd3ade4e1a650bfa05cfe
allen wes <allen.wes.g@gmail.com>
2015-05-13 11:05:44 -0500

    completed game with hard mode

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,27 @@
 function* guessingGame() {
-	console.log(yield game.say('Hey there!'));
-	console.log(yield game.say('Woah', 'This is cool'));
-	console.log(yield game.ask('Woah', 'This is cool'));
-	console.log(yield game.choose('Woah', 'This is cool', 'Hello world'));
-	console.log(yield game.say('That\'s all folks!', 'THE END'));
+	//console.log(yield game.say('Hey there!'));
+	//console.log(yield game.say('Woah', 'This is cool'));
+	//console.log(yield game.ask('Woah', 'This is cool'));
+	//console.log(yield game.choose('Woah', 'This is cool', 'Hello world'));
+	//console.log(yield game.say('That\'s all folks!', 'THE END'));
+	do {
+		var playAgain = "yes";
+		var number = Math.floor(Math.random()*100 + 1);
+		console.log(number);
+		yield game.say("I'm thinking of a number 1 through 100...");
+		var myGuess = yield game.ask("What is it?");
+		while (myGuess != number){
+			if (myGuess < number){
+				myGuess = yield game.ask("Your guess was too low! Guess again...");
+			} else {
+				myGuess = yield game.ask(" Your guess was too high! Guess again..");
+			}
+		}
+		if(myGuess == number){
+			yield game.say("Amazing guess! You Win!");
+			playAgain = yield game.choose("Do you wish to play again?","yes","no");
+		} 
+	} while (playAgain === "yes");
+
+	yield game.say("Hope you enjoyed this guessing game!");
 }
