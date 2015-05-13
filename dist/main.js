fa54229ab24594ec4112445ed722a61da8070931
allen wes <allen.wes.g@gmail.com>
2015-05-13 11:24:46 -0500

    took out extra stuff that wasnt needed

--- dist/main.js
+++ dist/main.js
@@ -5,11 +5,11 @@ function* guessingGame() {
 	//console.log(yield game.choose('Woah', 'This is cool', 'Hello world'));
 	//console.log(yield game.say('That\'s all folks!', 'THE END'));
 	do {
+
 		var playAgain = "yes";
 		var number = Math.floor(Math.random()*100 + 1);
-		console.log(number);
-		yield game.say("I'm thinking of a number 1 through 100...");
-		var myGuess = yield game.ask("What is it?");
+		var myGuess = yield game.ask("I'm thinking of a number 1 through 100... What is it?");
+
 		while (myGuess != number){
 			if (myGuess < number){
 				myGuess = yield game.ask("Your guess was too low! Guess again...");
@@ -17,10 +17,9 @@ function* guessingGame() {
 				myGuess = yield game.ask(" Your guess was too high! Guess again..");
 			}
 		}
-		if(myGuess == number){
-			yield game.say("Amazing guess! You Win!");
-			playAgain = yield game.choose("Do you wish to play again?","yes","no");
-		} 
+
+		playAgain = yield game.choose("Amazing guess! You Win! Do you wish to play again?","yes","no");
+
 	} while (playAgain === "yes");
 
 	yield game.say("Hope you enjoyed this guessing game!");
